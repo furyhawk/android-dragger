afc19a65ccb73d8d9f382dbe74994a04585f7722
furyhawk <furyx@hotmail.com>
2020-04-30 08:27:03 +0800

    step11

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -3,13 +3,16 @@ package com.example.android.dagger.di
 import android.content.Context
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.RegistrationComponent
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 // Definition of a Dagger component
 @Singleton
-@Component(modules = [StorageModule::class])
+@Component(modules = [StorageModule::class, AppSubcomponents::class])
 interface AppComponent {
     // Factory to create instances of the AppComponent
     @Component.Factory
@@ -18,6 +21,10 @@ interface AppComponent {
         fun create(@BindsInstance context: Context): AppComponent
     }
     // Classes that can be injected by this Component
-    fun inject(activity: RegistrationActivity)
+//    fun inject(activity: RegistrationActivity)
+//    fun inject(fragment: EnterDetailsFragment)
+//    fun inject(fragment: TermsAndConditionsFragment)
+    // Expose RegistrationComponent factory from the graph
+    fun registrationComponent(): RegistrationComponent.Factory
     fun inject(activity: MainActivity)
 }
