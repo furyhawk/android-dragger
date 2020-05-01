b947f2cf4ff986f784532155ca827fff3632635f
furyhawk <furyx@hotmail.com>
2020-05-01 11:21:22 +0800

    step15

--- app/src/main/java/com/example/android/dagger/MyApplication.kt
+++ app/src/main/java/com/example/android/dagger/MyApplication.kt
@@ -27,9 +27,12 @@ open class MyApplication : Application() {
     val appComponent: AppComponent by lazy {
         // Creates an instance of AppComponent using its Factory constructor
         // We pass the applicationContext that will be used as Context in the graph
-        DaggerAppComponent.factory().create(applicationContext)
+//        DaggerAppComponent.factory().create(applicationContext)
+        initializeComponent()
     }
-
+    open fun initializeComponent(): AppComponent {
+        return DaggerAppComponent.factory().create(applicationContext)
+    }
 //    open val userManager by lazy {
 //        UserManager(SharedPreferencesStorage(this))
 //    }
